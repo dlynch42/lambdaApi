3cc2a6429c0e22c58b39a2a3979e2cebcde0f049
Devin Lynch <djlynch17@gmail.com>
2023-10-12 14:22:13 -0700

    Update test & output location

--- lambda_test.sh
+++ lambda_test.sh
@@ -1,9 +1,9 @@
 # Invoke lambda function via API Gateway
 # Test 1
-aws lambda invoke --function-name automodel --payload file://aapl.txt output_aapl.txt 
+aws lambda invoke --function-name automodel --payload file://aapl.txt output/output_aapl.txt 
 
 # Test 2
-aws lambda invoke --function-name automodel --payload file://tsla.txt output_tsla.txt 
+aws lambda invoke --function-name automodel --payload file://tsla.txt output/output_tsla.txt 
 
 # Test 3
-aws lambda invoke --function-name automodel --payload file://amzn.txt output_amzn.txt 
+aws lambda invoke --function-name automodel --payload file://amzn.txt output/output_amzn.txt 
